753057ba12e798d9587f00d78a11ac1f198de69e
hanziqiang <1842002832@qq.com>
2018-10-30 15:48:24 +0800

    驾驶员信息管理

--- src/main/java/com/zjts/buscenter/common/config/CorsConfig.java
+++ src/main/java/com/zjts/buscenter/common/config/CorsConfig.java
@@ -1,32 +1,25 @@
-//package com.zjts.buscenter.common.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-///**
-// * 说明：跨域请求
-// *
-// * @author WangBin
-// * @version v1.0
-// * @date 2018/1/21/
-// */
-//@Configuration
-//@EnableWebMvc
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        //设置允许跨域的路径
-//        registry.addMapping("/**")
-//                //设置允许跨域请求的域名
-//                .allowedOrigins("*")
-//                //是否允许证书 不再默认开启
-//                .allowCredentials(true)
-//                //设置允许的方法
-//                .allowedMethods("GET", "POST", "DELETE", "PUT","PATCH")
-//                //跨域允许时间
-//                .maxAge(3600);
-//    }
-//}
+package com.zjts.buscenter.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+}
